51463c6e4df5b423f73eaab7e103d1eb8866f6b2
Christopher Sandoval <san13660@uvg.edu.gt>
2020-08-04 01:07:00 -0600

    Arreglar telefonos

--- csv_merger.py
+++ csv_merger.py
@@ -10,5 +10,5 @@ all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 if('todos_departamentos.csv' in all_filenames):
     all_filenames.remove('todos_departamentos.csv')
 
-combined_csv = pd.concat([pd.read_csv(f, encoding='ANSI') for f in all_filenames ])
+combined_csv = pd.concat([pd.read_csv(f, dtype=object, encoding='ANSI') for f in all_filenames ])
 combined_csv.to_csv( "todos_departamentos.csv", index=False, encoding='ANSI')
